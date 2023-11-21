39e91a95117f12dde65774e8f41c65ae06b14099
Mammad Mammadov <mmammadov194@gmail.com>
2023-11-21 23:28:31 +0400

    Complete first two subtasks

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-fetch('https://dummyjson.com/products')
-.then(res => res.json())
-.then(console.log)
-.catch(error => console.error('Error:', error));
+fetch("https://dummyjson.com/products")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const products = data.products
+    // console.log(products);
+    displayProducts(products);
+  })
+  .catch((error) => {
+    console.error("Error while getting the data", error);
+  });
+
+function displayProducts(products) {
+
+  const productList = document.getElementById("productList");
+
+  const productArray = Array.from(products);
+
+  productArray.forEach((product) => {
+
+    const productElement = document.createElement("div");
+
+    productElement.classList.add("product");
+    productElement.classList.add("grid-item");
+
+    productElement.innerHTML = `
+                <h3>${product.title}</h3>
+                <p>Price: $${product.price}</p>
+                <p>Discount: ${product.discountPercentage}%</p>
+                <p>Category: ${product.category}</p>
+                <p>Stock: ${product.stock}</p>
+                <img src="${product.thumbnail}" alt="${product.title}">`;
+
+    productList.appendChild(productElement);
+  });
+}
