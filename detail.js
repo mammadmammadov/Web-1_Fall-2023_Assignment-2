92072694fb7a8e0a0479880fe5abf82c5bced76a
Mammad Mammadov <mmammadov194@gmail.com>
2023-11-24 23:24:21 +0400

    OK

--- detail.js
+++ detail.js
@@ -5,26 +5,20 @@ fetch("https://dummyjson.com/products")
   .then((data) => {
     const products = data.products
     // console.log(products);
-    displayProducts(products);
+    displayProduct(products);
   })
   .catch((error) => {
     console.error("Error while getting the data", error);
   });
 
+function displayProduct(product) {
 
 
-function displayProducts(products) {
-
-  const productList = document.getElementById("productList");
-
-  const productArray = Array.from(products);
-
-  productArray.forEach((product) => {
+    const pro = document.getElementById("product-detail");
 
     const productElement = document.createElement("div");
+    
 
-    productElement.classList.add("product");
-    productElement.classList.add("grid-item");
 
     productElement.innerHTML = `
                 <h3>${product.title}</h3>
@@ -34,20 +28,9 @@ function displayProducts(products) {
                 <p>Stock: ${product.stock}</p>
                 <img src="${product.thumbnail}" alt="${product.title}">`;
 
-    productList.appendChild(productElement);
+    pro.appendChild(productElement);
 
-  });
-}
+  };
 
-  $(".product").click(function(){
-    $(document).on("click" , function() {
-      $(this).addClass(".orange");
-  });
-  })
 
-  document.getElementsByClassName(".product").addEventListener(
-    "click", func, false);
 
-  function func(){
-    console.log("Hello");
-  }
